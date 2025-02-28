a7aafb996db2123fdf2504d6a5f6da0898f39d0c
Sigurd Björne-Fagerli <sigfager@gmail.com>
2025-02-28 17:44:24 +0100

    Created static folder and template folder. Created base template and styling. Updated urls and linked static and templates in settings. Created an admin user.

--- django/visi/app/views.py
+++ django/visi/app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, "home.html")
+
+
+def events(request):    
+    return render(request, "events.html")
+
+def about(request):
+    return render(request, "about.html")
+
+def profile(request):
+    return render(request, "profile.html")
+
+def signup(request):
+    return render(request, "signup.html")
--- django/visi/visi/urls.py
+++ django/visi/visi/urls.py
@@ -17,7 +17,13 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from app import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", views.home, name="home"),
+    path("events/", views.events, name="events"),
+    path("about/", views.about, name="about"),
+    path("profile/", views.profile, name="profile"),
+    path("signup/", views.signup, name="signup"),
 ]

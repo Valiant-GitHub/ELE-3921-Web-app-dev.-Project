c5644ec662548f14aa7bbca9d334e141b52b9cdd
Vilijam Cekov <vilijamc@hotmail.com>
2025-05-12 18:17:14 +0200

    fixed admin.py conflicts

--- django/visi/app/admin.py
+++ django/visi/app/admin.py
@@ -10,14 +10,10 @@ admin.site.register(Fan)
 admin.site.register(Doorman)
 admin.site.register(Artist)
 admin.site.register(Venue)
-<<<<<<< HEAD
 admin.site.register(Photoreel)
 admin.site.register(Location)
 admin.site.register(Availability)
 admin.site.register(Events)
 admin.site.register(EventArtists)
 admin.site.register(Tickets)
-=======
-admin.site.register(Doorman)
-admin.site.register(Photoreel)
->>>>>>> eaae3bb75a28b19cda02dc19e5275edbe8743f83
+
